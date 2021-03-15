876e48bab53f4f158cfa2050d79cbf8cb7b80556
Florian Mathieu <fmathieu@agendize.com>
2021-03-15 10:48:50 +0100

    full report reseller-date mois precedent doffice

--- js/resellerReport1.js
+++ js/resellerReport1.js
@@ -9,13 +9,30 @@ var $_GET = $_GET(),
     endDate2 = "&endStart=" + endDate
     console.log(startDate)
     console.log(endDate)
+
+    var date = new Date();
+    date.setDate(0);
+    dateEnd = date.toISOString();
+    dateEnd3 = date.toLocaleDateString()
+    console.log(dateEnd3)
+    var x = new Date();
+    x.setDate(0); // 0 will result in the last day of the previous month
+    x.setDate(1);
+    dateStart = x.toISOString();
+    dateStart3 = x.toLocaleDateString()
+
     if (startDate == "" && endDate == "") {
-        startDate2 = "";
-        endDate2 = ""
+        startDate2 = dateStart
+        endDate2 = dateEnd
     }
+    
     if (title2 == 'undefined') {
         title2 = ""
     }
+    startDate.toLocaleString()
+    console.log(startDate2)
+
+    console.log(endDate2)
     function $_GET(param) {
 
         var vars = {};
@@ -35,7 +52,8 @@ var $_GET = $_GET(),
 function resellerReport1() {
     trieDef = []
     var xhr = new XMLHttpRequest();
-    var url = "https://"+ server+".agendize.com/api/2.0/resellers/reporting/full?apikey=" + apiKey + "&token=" + token + startDate2 + endDate2;
+    var url = "https://"+ server+".agendize.com/api/2.0/resellers/reporting/full?apikey=" + apiKey + "&token=" + token + "&startDate=" + startDate2 + "&endDate=" + endDate2;
+    console.log(url)
     xhr.open("GET", url, false);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onreadystatechange = function () {
@@ -46,7 +64,7 @@ function resellerReport1() {
             console.log(json)
 
 
-            document.write("<!DOCTYPE html><html lang='fr'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><link rel='stylesheet' href='style.css'><link rel='preconnect' href='https://fonts.gstatic.com'><link href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&family=Poppins&display=swap' rel='stylesheet'><link href='https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap' rel='stylesheet'><script src='https://code.jquery.com/jquery-3.5.1.min.js' integrity='sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=' crossorigin='anonymous'></script><title>Reseller|full report</title><link rel='stylesheet' href='style.css'></head><body><div id='divLogo'><img id='logo' src='https://developers.agendize.com/shared/img/logoaz.png' alt=''></div><div class='violet'><h1>" + title2 +"  </h1><h1>" + startDate + " - " + endDate + "</h1></div><div id='table'><table id=example><thead><tr style='color:black;font-weight:bold;background-color:#9370CB;'><th>owner</th><th>login</th><th>Email notifications sent</th><th>Finished appointment</th><th>New appointments</th><th>Text message notifications sent</th><th>SMS sent count</th><th>Marketing email sent</th><th>Marketing text message sent</th><th>New clients</th><th>Form</th><th>API</th><th>Call tracking duration</th><th>Call tracking numbers</th><th>Click to Call duration</th><th>Click to call buttons</th></tr></thead>");
+            document.write("<!DOCTYPE html><html lang='fr'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><link rel='stylesheet' href='style.css'><link rel='preconnect' href='https://fonts.gstatic.com'><link href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&family=Poppins&display=swap' rel='stylesheet'><link href='https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap' rel='stylesheet'><script src='https://code.jquery.com/jquery-3.5.1.min.js' integrity='sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=' crossorigin='anonymous'></script><title>Reseller|full report</title><link rel='stylesheet' href='style.css'></head><body><div id='divLogo'><img id='logo' src='https://developers.agendize.com/shared/img/logoaz.png' alt=''></div><div class='violet'><h1>" + title2 +"  </h1><h1>" + dateStart3.toLocaleString() + " - " + dateEnd3.toLocaleString() + "</h1></div><div id='table'><table id=example><thead><tr style='color:black;font-weight:bold;background-color:#9370CB;'><th>owner</th><th>login</th><th>Email notifications sent</th><th>Finished appointment</th><th>New appointments</th><th>Text message notifications sent</th><th>SMS sent count</th><th>Marketing email sent</th><th>Marketing text message sent</th><th>New clients</th><th>Form</th><th>API</th><th>Call tracking duration</th><th>Call tracking numbers</th><th>Click to Call duration</th><th>Click to call buttons</th></tr></thead>");
             document.write("<tbody>");
             odd = false;
             
@@ -86,6 +104,8 @@ function resellerReport1() {
 };
 xhr.send();
 }
+
+
 
 resellerReport1(apiKey,token,server,startDate,endDate);
 
